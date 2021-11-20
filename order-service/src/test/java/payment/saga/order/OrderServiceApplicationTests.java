d6db6c5a738a71e3850c7df1afdfd3aa33dce672
Martín Alexis Samán Arata <martinsam1610@gmail.com>
2021-11-20 04:37:43 -0500

    feat: add project and documentation

---
+++ order-service/src/test/java/payment/saga/order/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package payment.saga.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+class OrderServiceApplicationTests {
+
+
+	void contextLoads() {
+	}
+
+}
